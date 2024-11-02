c03cd9084e4e23493d52139a2ebb6d460cb5f79f
Chaiyong Ragkhitwetsagul <cragkhit@gmail.com>
2024-11-02 22:13:43 +0000

    Removed other test cases.

--- test_calculator.py
+++ test_calculator.py
@@ -9,29 +9,7 @@ class TestCalculator(unittest.TestCase):
     def test_add(self):
         self.assertEqual(self.calc.add(1, 2), 3)
 
-
-    def test_add_negative(self):
-        self.assertEqual(self.calc.add(-1, 1), 0)
-
-    def test_add_negative2(self):
-        self.assertEqual(self.calc.add(-1, -1), -2)
-
-    def test_subtract(self):
-        self.assertEqual(self.calc.subtract(10, 5), 5)
-        self.assertEqual(self.calc.subtract(-1, 1), -2)
-        self.assertEqual(self.calc.subtract(-1, -1), 0)
-
-    def test_multiply(self):
-        self.assertEqual(self.calc.multiply(3, 7), 21)
-        self.assertEqual(self.calc.multiply(-1, 1), -1)
-        self.assertEqual(self.calc.multiply(-1, -1), 1)
-
-    def test_divide(self):
-        self.assertEqual(self.calc.divide(10, 2), 5)
-        self.assertEqual(self.calc.divide(-1, 1), -1)
-        self.assertEqual(self.calc.divide(-1, -1), 1)
-        with self.assertRaises(ValueError):
-            self.calc.divide(10, 0)
+    # Add the following test methods to the TestCalculator class:
 
 if __name__ == '__main__':
     unittest.main()
